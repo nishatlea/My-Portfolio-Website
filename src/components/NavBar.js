a35f257fa794beb4add695914afdcd852a57e508
nishatlea <nishataurora1956@gmail.com>
2023-06-14 16:42:33 +0600

    navbar created

---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import React from "react";
+export const NavBar = () => {
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={"public/dev_logo.png"} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+            </div>
+            <button className="vvd" onClick={() => console.log("connect")}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
