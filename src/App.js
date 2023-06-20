46e103d06d8127a5db0c0dbb9c0165e6c36ff686
nishatlea <nishataurora1956@gmail.com>
2023-06-20 11:22:52 +0600

    projects section added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { NavBar } from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
@@ -32,12 +36,36 @@ export const Skills = () => {
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry.
+                <br></br> Lorem Ipsum has been the industry's standard dummy
+                text.
               </p>
-              <Carousel></Carousel>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skill-slider"
+              >
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="Image" />
+                  <h5>Brand Identity</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="Image" />
+                  <h5>Logo Design</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>Web Development</h5>
+                </div>
+              </Carousel>
             </div>
           </Col>
         </Row>
       </Container>
+      <img className="background-image-left" src={colorSharp} />
     </section>
   );
 };
