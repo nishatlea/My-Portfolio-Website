9eeeeccbc0c66b8847d3fa30f3c6f688d40ed7cc
nishat <nishataurora1956@gmail.com>
2023-10-30 14:50:11 +0600

    social app link included

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -69,13 +69,13 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#">
+              <a href="https://www.linkedin.com/in/nishat-anjum-lea-2020">
                 <img src={navIcon1} alt="" />
               </a>
-              <a href="#">
+              <a href="https://www.facebook.com">
                 <img src={navIcon2} alt="" />
               </a>
-              <a href="#">
+              <a href="https://www.instagram.com/">
                 <img src={navIcon3} alt="" />
               </a>
             </div>
